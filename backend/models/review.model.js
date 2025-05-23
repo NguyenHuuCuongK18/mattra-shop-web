127d0f63069634063c411a6fc7172214e4b5ef59
Leon <cuongnhhe186494@fpt.edu.vn>
2025-05-23 22:37:08 +0700

    Modified: OpenAPI documentation

--- backend/models/review.model.js
+++ backend/models/review.model.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 
-const reviewSchema = new mongoose.Schema({
-  productId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    required: true,
+const reviewSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    comment: { type: String, required: true },
   },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  rating: { type: Number, required: true, min: 1, max: 5 },
-  comment: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Review", reviewSchema);
--- backend/models/voucher.model.js
+++ backend/models/voucher.model.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
-const voucherSchema = new mongoose.Schema({
-  code: { type: String, unique: true },
-  discount_percentage: { type: Number, required: true, min: 0, max: 100 },
-  max_discount: { type: Number, required: true, min: 0 },
-  is_used: { type: Boolean, default: false },
-  subscriberOnly: { type: Boolean, default: false },
-  created_at: { type: Date, default: Date.now },
-  expires_at: { type: Date, required: true },
-});
+const voucherSchema = new mongoose.Schema(
+  {
+    code: { type: String, unique: true },
+    discount_percentage: { type: Number, required: true, min: 0, max: 100 },
+    max_discount: { type: Number, required: true, min: 0 },
+    is_used: { type: Boolean, default: false },
+    subscriberOnly: { type: Boolean, default: false },
+    expires_at: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Voucher", voucherSchema);
